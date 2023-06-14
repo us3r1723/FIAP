c72fca4aa93033fbc880bf0faf63dd6414a10e6c
us3r1723 <83931551+us3r1723@users.noreply.github.com>
2023-06-14 18:06:12 -0300

    Add files via upload

--- mk2.java
+++ mk2.java
@@ -8,7 +8,7 @@ public class mk2 {
 		// TODO Auto-generated method stub
 		Scanner entrada = new Scanner(System.in);
 		char jogo = 'z';
-		int i = 0, var = 0, valor = 0, ener = 0, bateu = 0, venceu = 0, jogoInt = 1, vida = 3;
+		int i = 0, var = 0, valor = 0, ener = 0, bateu = 0, venceu = 0, jogoInt = 1, vida = 9;
 		// var = direcao
 		// var=1 direita
 		// var=2 esquerda
@@ -75,8 +75,7 @@ public class mk2 {
 			System.out.println("então tome coidado com o numero de instruções");
 			System.out.println("");
 			System.out.println("");
-			System.out.println(
-					"voce tera 3 vidas quando vc bater ele retornara para o local antes da intruçao que o vez bater ");
+			System.out.println("voce tera 3 vidas quando vc bater ele retornara para o local antes da intruçao que o vez bater ");
 			System.out.println("");
 			System.out.println("digite a potência da bateria do robô em J");
 			System.out.println("lembrando que se não for o suficiente ela vai acabar no meio do caminho");
@@ -165,9 +164,9 @@ public class mk2 {
 								if (var == 2 && i == 12 && i - valor >= 0) {
 									i -= valor;
 								} else {
-									if (var == 1) {
-										vida -= 1;
-									}
+
+									vida -= 1;
+
 								}
 							}
 						}
@@ -218,9 +217,9 @@ public class mk2 {
 								if (var == 3 && i == 21 && i - valor >= 12) {
 									i -= valor;
 								} else {
-									if (var == 4) {
-										vida -= 1;
-									}
+
+									vida -= 1;
+
 								}
 							}
 						}
@@ -271,13 +270,16 @@ public class mk2 {
 								if (var == 2 && i == 33 && i - valor >= 21) {
 									i -= valor;
 								} else {
-									if (var == 4) {
-										vida -= 1;
-									}
+									vida -= 1;
+
 								}
+
 							}
 						}
+						if (vida <= 0 || ener < 0) {
+							bateu++;
 
+						}
 					} else {
 						System.out.println("valores inválidos");
 					}
@@ -319,13 +321,16 @@ public class mk2 {
 								if (var == 4 && i == 39 && i - valor >= 33) {
 									i -= valor;
 								} else {
-									if (var == 3) {
-										vida -= 1;
-									}
+
+									vida -= 1;
+
 								}
 							}
 						}
+						if (vida <= 0 || ener < 0) {
+							bateu++;
 
+						}
 					} else {
 						System.out.println("valores inválidos");
 					}
@@ -368,13 +373,16 @@ public class mk2 {
 								if (var == 2 && i == 51 && i - valor >= 39) {
 									i -= valor;
 								} else {
-									if (var == 1) {
-										vida -= 1;
-									}
+
+									vida -= 1;
+
 								}
 							}
 						}
+						if (vida <= 0 || ener < 0) {
+							bateu++;
 
+						}
 					} else {
 						System.out.println("valores inválidos");
 					}
